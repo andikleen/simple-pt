6cc94cfaf003c73bd744b2d6922fc28ccfec7748
Andi Kleen <ak@linux.intel.com>
2014-08-09 14:38:41 -0700

    remove obsolete ioctls

--- simple-pt.h
+++ simple-pt.h
@@ -1,8 +1,6 @@
 
 
 #define SIMPLE_PT_SET_CPU 1 /* arg: cpu number */
-#define SIMPLE_PT_START 2   /* global start */
-#define SIMPLE_PT_STOP 3    /* global stop */
-#define SIMPLE_PT_GET_SIZE 4 /* arg: pointer to int, output for buffer size */
-#define SIMPLE_PT_GET_OFFSET 5 /* arg: pointer to int, last write offset in buffer */
+#define SIMPLE_PT_GET_SIZE 2 /* arg: pointer to int, output for buffer size */
+#define SIMPLE_PT_GET_OFFSET 3 /* arg: pointer to int, last write offset in buffer */
 			     /* must be stopped */
