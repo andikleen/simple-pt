7ce5b89df080415a806a90dcebb49e495d42241d
Andi Kleen <ak@linux.intel.com>
2014-08-10 09:09:40 -0700

    symoffset: Add last hit cache

--- symoffset.py
+++ symoffset.py
@@ -25,6 +25,7 @@ for l in pipe:
                 addresses.insert(pos, val)
 		symtab.insert(pos, m.group(2))
 
+last_ndx = None
 for l in sys.stdin:
 	n = l.split()
         try:
@@ -32,13 +33,19 @@ for l in sys.stdin:
         except:
             print l,
             continue
-	n = bisect.bisect_left(addresses, adr)
-	if n >= len(addresses):
+        # last hit cache
+        if (last_ndx and adr >= addresses[last_ndx] and
+                (last_ndx == len(addresses)-1 or adr < addresses[last_ndx + 1])):
+            n = last_ndx
+        else:
+	    n = bisect.bisect_left(addresses, adr)
+	    if n >= len(addresses):
 		#print "%u not found %d/%d" % (adr,n,len(symtab))
 		print l,
 		continue
-	sym = symtab[n]
-	print "%-30s" % ("%s+%d" % (symtab[n], -(adr - addresses[n]))),
+            last_ndx = n
+	    sym = symtab[n]
+	print "%-40s" % ("%s+%d" % (symtab[n], -(adr - addresses[n]))),
         print l.rstrip(),
         m = re.search(r'\[rip\+(0x[0-9a-f]+)\]', l)
         if m:
