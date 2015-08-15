07aa0b831b5e0c2297ee930a267c62ff924db9aa
Andi Kleen <ak@linux.intel.com>
2015-08-14 19:09:33 -0700

    driver: use 64bit register constants

--- simple-pt.c
+++ simple-pt.c
@@ -72,16 +72,16 @@
 #define MSR_IA32_RTIT_OUTPUT_BASE	0x00000560
 #define MSR_IA32_RTIT_OUTPUT_MASK_PTRS	0x00000561
 #define MSR_IA32_RTIT_CTL		0x00000570
-#define TRACE_EN	BIT(0)
-#define CYC_EN		BIT(1)
-#define CTL_OS		BIT(2)
-#define CTL_USER	BIT(3)
-#define CR3_FILTER	BIT(7)
-#define TO_PA		BIT(8)
-#define MTC_EN		BIT(9)
-#define TSC_EN		BIT(10)
-#define DIS_RETC	BIT(11)
-#define BRANCH_EN	BIT(13)
+#define TRACE_EN	BIT_ULL(0)
+#define CYC_EN		BIT_ULL(1)
+#define CTL_OS		BIT_ULL(2)
+#define CTL_USER	BIT_ULL(3)
+#define CR3_FILTER	BIT_ULL(7)
+#define TO_PA		BIT_ULL(8)
+#define MTC_EN		BIT_ULL(9)
+#define TSC_EN		BIT_ULL(10)
+#define DIS_RETC	BIT_ULL(11)
+#define BRANCH_EN	BIT_ULL(13)
 #define MTC_MASK	(0xf << 14)
 #define CYC_MASK	(0xf << 19)
 #define PSB_MASK	(0xf << 24)
@@ -89,9 +89,9 @@
 #define ADDR1_MASK	(0x3ULL << 36)
 #define MSR_IA32_RTIT_STATUS		0x00000571
 #define MSR_IA32_CR3_MATCH		0x00000572
-#define TOPA_STOP	BIT(4)
-#define TOPA_INT	BIT(2)
-#define TOPA_END	BIT(0)
+#define TOPA_STOP	BIT_ULL(4)
+#define TOPA_INT	BIT_ULL(2)
+#define TOPA_END	BIT_ULL(0)
 #define TOPA_SIZE_SHIFT 6
 #define MSR_IA32_ADDR0_START		0x00000580
 #define MSR_IA32_ADDR0_END		0x00000581
